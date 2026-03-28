4ef1784f30aa7555c75f13ed860666886faf6812
Alan Albert <alanalbertmn@gmail.com>
2026-03-27 22:15:10 -0600

    Added cart functionality for adding and deleting one by one

--- src/components/cartdrawer.js
+++ src/components/cartdrawer.js
@@ -1,50 +1,104 @@
-import { Minus, MinusCircleIcon, MinusIcon, MinusSquare, Plus, PlusCircle, PlusCircleIcon, PlusSquare, X } from 'lucide-react';
+import { MinusCircleIcon, PlusCircleIcon, Trash, X } from 'lucide-react';
 import { useCart } from '@/lib/CartProvider';
-import { useEffect } from 'react';
 
 const CartDrawer = ({ handleCartOpen }) => {
-    const { cart, setCart } = useCart();
+	const { cart, setCart } = useCart();
 
-    // const [cart, setCart] = useState(cartContext);
+	// const [cart, setCart] = useState(cartContext);
 
-    return (
-        <div className='fixed inset-0 bg-black/50 z-50'>
-            <div className='absolute right-0 top-0 h-full w-80 bg-slate-900 p-6 overflow-y-auto'>
-                <div className='flex justify-between mb-6'>
-                    <h2>Cart</h2>
-                    <button onClick={() => handleCartOpen(false)}>
-                        <X />
-                    </button>
-                </div>
+	const handleProductAdd = item => {
+		setCart(
+			cart.map(ci =>
+				ci.product.asin == item.product.asin
+					? { ...item, quantity: item.quantity + 1 }
+					: ci,
+			),
+		);
+	};
 
-                {cart.length === 0 && <p>No items</p>}
+	const handleProductDelete = item => {
+		setCart(prevCart => {
+			if (item.quantity == 1) {
+				return prevCart.filter(ci => ci.product.asin != item.product.asin);
+			} else {
+				return prevCart.map(ci => {
+					if (ci.product.asin == item.product.asin) {
+						return { ...ci, quantity: ci.quantity - 1 };
+					} else return ci;
+				});
+			}
+		});
+	};
 
-                {cart.map((item, i) => (
+	return (
+		<div className='fixed inset-0 bg-black/50 z-50'>
+			<div className='absolute right-0 top-0 h-full w-80 bg-slate-900 p-6 overflow-y-auto'>
+				<div className='flex justify-between mb-6'>
+					<h2>Cart</h2>
+					<button onClick={() => handleCartOpen(false)}>
+						<X />
+					</button>
+				</div>
+
+				{cart.length === 0 && <p>No items</p>}
+
+				{cart.map((item, i) => (
 					<div key={i} className='my-4 flex flex-col items-center'>
-                        <p className='text-center'>{`${item.product.product_title.split(' ').slice(0,10).join(' ')}`}</p>
-                        <p className='text-sm text-slate-400'>
-                            {item.product.product_price}
-                        </p>
-                        <div className='flex gap-4'>
-							<MinusCircleIcon className='cursor-pointer'/>
-                            <p>{item.quantity}</p>
-							<PlusCircleIcon className='cursor-pointer'/>
-                        </div>
-                    </div>
-                ))}
+						<p className='text-center'>{`${item.product.product_title.split(' ').slice(0, 10).join(' ')}`}</p>
+						<p className='text-sm text-slate-400'>
+							{item.product.product_price}
+						</p>
+						<div className='flex gap-4'>
+							{item.quantity == 1 ? (
+								<button>
+									<Trash
+										className='cursor-pointer'
+										onClick={() => handleProductDelete(item)}
+									/>
+								</button>
+							) : (
+								<button>
+									<MinusCircleIcon
+										className='cursor-pointer'
+										onClick={() => handleProductDelete(item)}
+									/>
+								</button>
+							)}
+							<p className=''>{item.quantity}</p>
+
+							<button>
+								<PlusCircleIcon
+									className='cursor-pointer'
+									onClick={() => handleProductAdd(item)}
+								/>
+							</button>
+						</div>
+					</div>
+				))}
 				<p className='text-center text-2xl'>
-					Total: ${cart.reduce((cartTotal, currProd) => cartTotal + (currProd.product.product_price.replaceAll('$', '')*currProd.quantity), 0)}
+					Total: $
+					{cart
+						.reduce(
+							(cartTotal, currProd) =>
+								cartTotal +
+								currProd.product.product_price.replaceAll('$', '') *
+									currProd.quantity,
+							0,
+						)
+						.toFixed(2)}
 				</p>
-                <button className='mt-6 w-full bg-blue-500 py-3 rounded-xl'>
-                    Checkout
-                </button>
+				<button className='mt-6 w-full bg-blue-500 py-3 rounded-xl'>
+					Checkout
+				</button>
 				<button
 					className='bg-red-800 rounded-xl w-full py-2 mt-6'
 					onClick={() => setCart([])}
-				>Clear cart</button>
-            </div>
-        </div>
-    );
+				>
+					Clear cart
+				</button>
+			</div>
+		</div>
+	);
 };
 
 export default CartDrawer;
